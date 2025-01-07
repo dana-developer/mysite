4561a11aab530c8d5d22342874abb8481d6a03f8
dana-developer <hdy78901@naver.com>
2025-01-07 12:56:36 +0900

    mysite03 실행시간 측정 aop #1

--- mysite03/src/main/java/mysite/repository/UserRepository.java
+++ mysite03/src/main/java/mysite/repository/UserRepository.java
@@ -7,7 +7,6 @@ import javax.sql.DataSource;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.util.StopWatch;
 
 import mysite.vo.UserVo;
 
@@ -28,17 +27,7 @@ public class UserRepository {
 	}
 	
 	public UserVo findByEmailAndPassword(String email, String password) {
-		StopWatch sw = new StopWatch();
-		sw.start();
-		
-		UserVo userVo = sqlSession.selectOne("user.findByEmailAndPassword", Map.of("email", email, "password", password));
-		
-		sw.stop();
-		
-		long totalTime = sw.getTotalTimeMillis();
-		System.out.println("[Execution Time][UserRepository.findByEmailAndPassword] " + totalTime + "mills"); // 로그
-		
-		return userVo;
+		return sqlSession.selectOne("user.findByEmailAndPassword", Map.of("email", email, "password", password));
 	}
 	
 	public UserVo findById(Long id) {
