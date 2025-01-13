308941790adf0ab9b69f42d4c2d322e1c5dec4b8
dana-developer <hdy78901@naver.com>
2025-01-13 11:22:19 +0900

    jsonResult, exception handler 수정 #1

--- mysite03/src/main/java/mysite/exception/GlobalExceptionHandler.java
+++ mysite03/src/main/java/mysite/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package mysite.exception;
 
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
@@ -9,19 +10,43 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import mysite.dto.JsonResult;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
 	private static final Log log = LogFactory.getLog(GlobalExceptionHandler.class);
-	
+
 	@ExceptionHandler(Exception.class)
-	public String handler(Model model, Exception e) {
+	public void handler(HttpServletRequest request, HttpServletResponse response, Model model, Exception e) throws Exception {
+
 		// 1. logging
 		StringWriter errors = new StringWriter();
 		e.printStackTrace(new PrintWriter(errors));
 		log.error(errors.toString());
-		
-		// 2. 사과 페이지(종료)
-		model.addAttribute("errors", errors.toString());
-		return "errors/exception";
+
+		// 2. 요청 구분
+		// json 요청 : request header의 accept : application/json (o)
+		// html 요청 : request header의 accept : application/json (x)
+		String accept = request.getHeader("accept");
+		if (accept.matches(".*application/json.*")) {
+			// 3. JSON 응답
+			JsonResult jsonResult = JsonResult.fail(errors.toString());
+			String jsonString = new ObjectMapper().writeValueAsString(jsonResult);
+			
+			response.setStatus(HttpServletResponse.SC_OK);
+			response.setContentType("application/json; charset=utf-8");
+			OutputStream os = response.getOutputStream();
+			os.write(jsonString.getBytes("utf-8"));
+			os.close();
+			
+		} else {
+			// 4. 사과 페이지(종료)
+			request.setAttribute("errors", errors.toString());
+			request.getRequestDispatcher("/WEB-INF/views/errors/exception.jsp").forward(request, response);
+		}
 	}
 }
