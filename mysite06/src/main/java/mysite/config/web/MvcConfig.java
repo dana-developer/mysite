7a5540997af20e23d8abbc906f32b2a5e9952942
dana-developer <hdy78901@naver.com>
2025-01-22 10:56:39 +0900

    mysite06 #2

--- mysite06/src/main/java/mysite/config/web/MvcConfig.java
+++ mysite06/src/main/java/mysite/config/web/MvcConfig.java
@@ -1,17 +1,7 @@
 package mysite.config.web;
 
-import java.nio.charset.Charset;
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.List;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -39,31 +29,6 @@ public class MvcConfig implements WebMvcConfigurer {
 		viewResolver.setExposedContextBeanNames("site");
 		return viewResolver;
 	}
-
-	// Message Converter
-	@Bean
-	public StringHttpMessageConverter stringHttpMessageConverter() {
-		StringHttpMessageConverter messageConverter = new StringHttpMessageConverter();
-		messageConverter.setSupportedMediaTypes(Arrays.asList(new MediaType("text", "html", Charset.forName("utf-8"))));
-		return messageConverter;
-	}
-
-	@Bean
-	public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
-		Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder().indentOutput(true)
-				.dateFormat(new SimpleDateFormat("yyyy-mm-dd hh:MM:ss"));
-
-		MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter(builder.build());
-		messageConverter
-				.setSupportedMediaTypes(Arrays.asList(new MediaType("application", "json", Charset.forName("utf-8"))));
-		return messageConverter;
-	}
-
-	@Override
-	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-		converters.add(stringHttpMessageConverter());
-		converters.add(mappingJackson2HttpMessageConverter());
-	}
 	
 	// static(assets) url mapping
 	@Override
@@ -71,12 +36,6 @@ public class MvcConfig implements WebMvcConfigurer {
 		registry.addResourceHandler("/assets/**")
 		.addResourceLocations("classpath:assets/");
 	}
-
-//	// DefaultServlet Handler
-//	@Override
-//	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-//		configurer.enable();
-//	}
 
 	// ApplicationContextEventListener
 	@Bean
